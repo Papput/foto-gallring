95b5c96c84559eeb45d31c6cdb7f5d55c6c7f3c1
Gunnar Abrahamsson <nvtesp@gmail.com>
2021-01-15 00:24:57 +0100

    Try to log in test

--- src/components/auth/login/Login.test.tsx
+++ src/components/auth/login/Login.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
 import App from '../../../App';
 import { BrowserRouter } from 'react-router-dom'
 import { createMemoryHistory } from 'history';
@@ -42,6 +43,44 @@ describe('Login component', () => {
             expect(PasswordInput).toBeInTheDocument();
         })
 
+        
+    });
+
+
+    test('Shows forgot password message', async () => {
+        const history = createMemoryHistory();
+        window.history.pushState({}, '', '/login')
+        act(() => {
+            render(
+                <Provider store={store}>
+                    <ReactReduxFirebaseProvider {...reduxFirebaseprops} >
+                        <BrowserRouter history={history}>
+                            <App />
+                        </BrowserRouter>
+                    </ReactReduxFirebaseProvider>
+                </Provider>
+            );
+        })
+
+        
+        
+        await waitFor(async () => {
+            const LoginButton = screen.getByRole('button', { name: /sign in/i});
+            const EmailInput = screen.getByRole('textbox', { name: 'Email address'});
+            const PasswordInput = screen.getByPlaceholderText(/password/i) 
+
+            // expect(EmailInput).toHaveValue('');
+            await userEvent.type(EmailInput, 'testuser@test.com', { delay: 10 });
+            expect(EmailInput).toHaveValue('testuser@test.com');
+
+            await userEvent.type(PasswordInput, '1231234', { delay: 10 });
+            expect(PasswordInput).toHaveValue('1231234');
+            userEvent.click(LoginButton)
+        })
+        
+        await waitFor(() => {
+            expect(screen.queryByRole('link', { name: 'Forgot password?' })).toBeInTheDocument();
+        })
         
     });
 })
